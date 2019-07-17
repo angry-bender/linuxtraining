9d9b1b87f7fa55310deefa3c0ceedce38b5066a9
samuel <samuel.freeman@caterpillar.git.local>
2019-07-17 14:53:57 +0930

    fixed bad for loop

--- foruser.sh
+++ foruser.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # use  MY_USERS for security, that way we wont overwrite a real environmental var
-MY_USERS=users.txt
-for user in "$(cat "$MY_USERS")"; do
+for user in $( cat users.txt ); do
 	useradd $user 
 done
